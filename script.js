ce60b3fc037a3ad5bce7f9e7824a3832ac4035c3
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2026-06-23 13:57:22 +0500

    Start working on image slider

---
+++ script.js
@@ -0,0 +1,39 @@
+var allImg = [
+    "https://images.unsplash.com/photo-1630148180214-417337ce9652?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1630694093867-4b947d812bf0?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1630839437035-dac17da580d0?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1663427929917-333d88949f7a?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701014159024-f9781490a228?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1630839437035-dac17da580d0?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1701486485832-6d3af9ca0e60?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1769364323382-e2de114ab151?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1639921884918-8d28ab2e39a4?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1543722530-d2c3201371e7?q=80&w=874&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+]
+let img = document.getElementById('img')
+let preBtn = document.getElementById('pre-Btn')
+let nxtBtn = document.getElementById('nxt-Btn')
+
+let i = 0;
+
+
+function Next() {
+    i = i + 1;
+    img.src = allImg[i];
+    if (i == allImg.length - 1) {
+        nxtBtn.disabled = true;
+    } else {
+        nxtBtn.disabled = false
+    }
+    preBtn.disabled = false
+}
+function previous() {
+    if (i === 1) {
+        preBtn.disabled = true
+    }
+    i = i - 1;
+    img.src = allImg[i];
+    nxtBtn.disabled = false
+}
+
+console.log(img.src);
